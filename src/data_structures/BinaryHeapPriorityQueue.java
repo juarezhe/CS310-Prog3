ba1accdead4b44cd1480f9d28caf1d5d54b387e4
whyrl <whyrl@DESKTOP-TV3Q4V5>
2019-04-06 18:05:20 -0700

    Create BinaryHeapPriorityQueue, minor text changes in PriorityQueue

--- src/data_structures/BinaryHeapPriorityQueue.java
+++ src/data_structures/BinaryHeapPriorityQueue.java
@@ -1,63 +1,86 @@
-/* 
- * Hannah Juarez
- * cssc1481
- */
-
-/*
- * The PriorityQueue ADT may store objects in any order. However,
- * removal of objects from the PQ must follow specific criteria.
- * The object of highest priority that has been in the PQ longest
- * must be the object returned by the remove() method. FIFO return
- * order must be preserved for objects of identical priority.
- * 
- * Ranking of objects by priority is determined by the Comparable<E>
- * interface. All objects inserted into the PQ must implement this
- * interface.
- */
-
 package data_structures;
 
 import java.util.Iterator;
 
-public interface PriorityQueue<E extends Comparable<E>> extends Iterable<E> {
-	public static final int DEFAULT_MAX_CAPACITY = 1000;
+public class BinaryHeapPriorityQueue<E extends Comparable<E>> implements PriorityQueue<E> {
 
 	// Inserts a new object into the priority queue. Returns true if
 	// the insertion is successful. If the PQ is full, the insertion
 	// is aborted, and the method returns false.
-	public boolean insert(E object);
+	@Override
+	public boolean insert(E object) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 	// Removes the object of highest priority that has been in the
 	// PQ the longest, and returns it. Returns null if the PQ is empty.
-	public E remove();
+	@Override
+	public E remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	// Deletes all instances of the parameter obj from the PQ if found, and
 	// returns true. Returns false if no match to the parameter obj is found.
-	public boolean delete(E obj);
+	@Override
+	public boolean delete(E obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 	// Returns the object of highest priority that has been in the
 	// PQ the longest, but does NOT remove it.
 	// Returns null if the PQ is empty.
-	public E peek();
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	// Returns true if the priority queue contains the specified element
 	// false otherwise.
-	public boolean contains(E obj);
+	@Override
+	public boolean contains(E obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 	// Returns the number of objects currently in the PQ.
-	public int size();
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	// Returns the PQ to an empty state.
-	public void clear();
-	
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
 	// Returns true if the PQ is empty, otherwise false
-	public boolean isEmpty();
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 	// Returns true if the PQ is full, otherwise false. List based
 	// implementations should always return false.
-	public boolean isFull();
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 	// Returns an iterator of the objects in the PQ, in no particular
 	// order.
-	public Iterator<E> iterator();
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
